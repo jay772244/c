8b2ecc241713b783fd74b32454a41e14103378a9
Jitendra Asole <jasole1111@gmail.com>
2022-04-05 08:46:48 +0530

    program for marks of maths of 5 students

---
+++ maths_marks/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    float a, b, c, d, e;
+    printf("Please enter the marks of 5 students one by one followed by an Enter Key");
+    scanf("%f", a);
+    scanf("%f", b);
+    scanf("%f", c);
+    scanf("%f", d);
+    scanf("%f", e);
+    printf("The marks of student A is %f", a);
+    printf("The marks of student A is %f", b);
+    printf("The marks of student A is %f", c);
+    printf("The marks of student A is %f", d);
+    printf("The marks of student A is %f", e);
+}
