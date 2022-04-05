d1e793c50dad1df7f071015d422915cffd0e5bfd
Jitendra Asole <jasole1111@gmail.com>
2022-04-05 08:56:18 +0530

    Modified the marks programs and brought to the main dir

---
+++ main.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+
+int main(){
+    int a, b, c, d, e;
+    printf("Please enter the marks of student A");
+    scanf("%d", a);
+    printf("Please enter the marks of student B");
+    scanf("%d", b);
+    printf("Please enter the marks of student C");
+    scanf("%d", c);
+    printf("Please enter the marks of student D");
+    scanf("%d", d);
+    printf("Please enter the marks of student E");
+    scanf("%d", e);
+    printf("The marks of student A is %d", a);
+    printf("The marks of student B is %d", b);
+    printf("The marks of student C is %d", c);
+    printf("The marks of student D is %d", d);
+    printf("The marks of student E is %d", e);
+    return 0;
+}
